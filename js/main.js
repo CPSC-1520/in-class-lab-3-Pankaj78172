b8b1798f58b57e6b664d076afb42e65b9411fdde
Pankaj78172 <pankajpaliwal61@gmail.com>
2024-10-02 21:53:58 -0600

    not completed

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
 // Enter your code below.
+
+let java = document.querySelector(".javascript-resources");
+let btn = document.querySelector("#show-resources");
+
+btn.addEventListener("click", function() {
+    java.classList.remove("d-none");
+});
+
+java.addEventListener("mouseover", function(event) {
+    if (event.target.classList.contains("list-group-item")) {
+        console.log(event.target);
+        event.target.classList.add("fw-bold");
+    }
+});
+
+java.addEventListener("mouseout", function(event) {
+    if (event.target.classList.contains("list-group-item")) {
+        event.target.classList.remove("fw-bold");
+    }
+});
+
+java.addEventListener("click", function(event) {
+    if (event.target.classList.contains("list-group-item")) {
+        event.target.classList.toggle("fst-italic"); 
+    }
+});
+
