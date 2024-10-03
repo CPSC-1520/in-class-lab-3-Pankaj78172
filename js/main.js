757a40d323c3ba8fe5457ea8d6dcdbc84318cc0d
Pankaj78172 <pankajpaliwal61@gmail.com>
2024-10-02 22:15:18 -0600

    done

--- js/main.js
+++ js/main.js
@@ -3,26 +3,23 @@
 let java = document.querySelector(".javascript-resources");
 let btn = document.querySelector("#show-resources");
 
-btn.addEventListener("click", function() {
+
+btn.addEventListener("click", function(){
     java.classList.remove("d-none");
 });
 
-java.addEventListener("mouseover", function(event) {
-    if (event.target.classList.contains("list-group-item")) {
-        console.log(event.target);
-        event.target.classList.add("fw-bold");
-    }
+java.addEventListener("mouseover",function(event){
+    console.log(event.target);
+
+        event.target.classList.add('fw-bold');
+     
 });
 
-java.addEventListener("mouseout", function(event) {
-    if (event.target.classList.contains("list-group-item")) {
-        event.target.classList.remove("fw-bold");
-    }
+java.addEventListener("mouseout",function(event){
+
+    event.target.classList.remove('fw-bold');
 });
 
-java.addEventListener("click", function(event) {
-    if (event.target.classList.contains("list-group-item")) {
-        event.target.classList.toggle("fst-italic"); 
-    }
-});
-
+java.addEventListener("click", function(event){
+    event.target.classList.toggle("fst-italic");
+})
